82dce6dfe734edc3bd110127bcbcb1f71efff824
Daniel Deng <36489683+danieldeng2@users.noreply.github.com>
2022-02-02 01:15:33 +0000

    change script to use await sleep

--- PerlegoContent.js
+++ PerlegoContent.js
@@ -1,4 +1,13 @@
-function loadImages() {
+function sleep(ms) {
+    return new Promise((resolve)=>setTimeout(resolve, ms));
+}
+
+function fixStyle(isPDF) {
+    if (!isPDF)
+        document.body.style.padding = "40px";
+    document.body.style["overflow-y"] = "scroll";
+
+    // load images
     document.querySelectorAll("img").forEach((e)=>{
         const imageName = e.className.split("-");
 
@@ -10,60 +19,59 @@ function loadImages() {
             e.style["max-width"] = "100%";
             e.style["max-height"] = "100%";
         }
-
     }
     );
+
+    // remove bootstrap
+    const headerLinks = document.querySelectorAll("head > link");
+    headerLinks.forEach((link)=>{
+        if ((link)=>link.href.matches(/.*bootstrap.*/)) {
+            link.remove();
+        }
+    }
+    )
 }
 
-function printPages(start, end) {
+async function printPages(start, end) {
     document.querySelector("style").remove();
     const isPDF = document.querySelector(".pdf-content") != undefined;
     let printContents = "";
-    let i = start;
-    function downloadPage() {
-        setTimeout(()=>{
-            let content;
 
-            for (let j = i; j < i + 10; j++) {
-                content = document.querySelector(`div[data-chapterid='${j}']`)
-                if (content) {
-                    i = j;
-                    break;
-                }
-            }
+    for (let i = start; i <= end; i++) {
+        let content;
 
-            if (!content || content.querySelector(".pdfplaceholder") != null) {
-                downloadPage();
-                return;
-            }
-            if (isPDF)
-                content.scrollIntoView();
-            printContents += content.innerHTML + "<div style='page-break-before: always;'></div>";
-            i++;
-            if (i <= end) {
-                if (!isPDF)
-                    document.querySelector(`button[data-test-locator='ChevronButton-next-chapter']`).click();
-                downloadPage();
-            }
-            if (i == end + 1) {
-                if (isPDF) {
-                    document.body.innerHTML = printContents;
-                } else {
-                    document.body.innerHTML = "<div style='padding:40px;'>" + printContents + "</div>";
-                }
-
-                console.log("Loading...");
-                setTimeout(()=>{
-                    loadImages();
-                    window.print();
-                }
-                , (end - start) * 100);
+        for (let j = i; j < i + 10; j++) {
+            content = document.querySelector(`div[data-chapterid='${j}']`)
+            if (content) {
+                i = j;
+                break;
             }
         }
-        , 100);
+
+        if (!content || content.querySelector(".pdfplaceholder") != null) {
+            i--;
+            await sleep(100);
+            continue;
+        }
+
+        printContents += content.innerHTML;
+
+        if (i !== end) {
+            printContents += "<div style='page-break-before: always;'></div>"
+
+            if (isPDF)
+                content.scrollIntoView();
+            else
+                document.querySelector(`button[data-test-locator='ChevronButton-next-chapter']`).click();
+        }
     }
-    downloadPage();
-    return "Reading...";
+
+    document.body.innerHTML = printContents;
+    fixStyle(isPDF);
+
+    console.log("Loading...");
+    await sleep((end - start + 1) * 200);
+    window.print();
 }
 
 "Enter printPages(startPage, endPage) to get started! ";
